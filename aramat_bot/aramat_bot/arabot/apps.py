c7c72548175e2a9b39aa3893261d9b7ecd6cbdf3
Alex <dmasalex@gmail.com>
2022-01-20 15:17:58 +0300

    remove code.py

--- aramat_bot/aramat_bot/arabot/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArabotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'arabot'
-    verbose_name = 'Телеграмм-бот'
--- flask/app/posts/blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from models import Post
-
-posts = Blueprint('posts', __name__, template_folder='templates')
-
-@posts.route('/')
-def index():
-    return render_template('posts/index.html')
